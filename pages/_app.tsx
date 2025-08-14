1ce3b13970453faac10b5c066af42e79546563ac
Jimmy <jimmy@Jimmys-Mac-mini.local>
2025-08-14 14:34:44 +1000

    add roboto to fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
